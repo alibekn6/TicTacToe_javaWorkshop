a405f8ddda45a2dcaa35d51a05c1d26b2247cf71
alibekN54 <alibekandanuarbek@gmail.com>
2023-02-16 23:24:15 +0600

    ticTac

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,13 @@
 // Created by Alibekn.INC;
+
 import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TicTacToe {
+    static ArrayList<Integer> PlayerPosition = new ArrayList<Integer>();
     public static void main(String[] args) {
         char[][] gameBoard = {
                 {' ', '|', ' ', '|', ' '},
@@ -25,7 +30,10 @@ public class TicTacToe {
             place(gameBoard, AIpos, "AI");
 
             printBoard(gameBoard);
+            scanner.close();
         }
+
+    
     }
 
     public static void printBoard(char[][] gameBoard) {
@@ -57,5 +65,21 @@ public class TicTacToe {
             case 8 -> gameBoard[4][2] = symbol;
             case 9 -> gameBoard[4][4] = symbol;
         }
+    }
+
+    public static String checkWinner() {
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9); 
+
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+
+        return "";        
     }
 }
